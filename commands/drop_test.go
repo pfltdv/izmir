d706529703c7441854256b33867c57112d49d2ef
Mustafa Kara <mkara@pinchflat.dev>
2023-08-18 20:24:58 +0300

    Implement drop command (#14)
    Signed-off-by: Mustafa Kara <mkara@pinchflat.dev>

--- commands/drop_test.go
+++ commands/drop_test.go
@@ -7,21 +7,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDiffNoRoot(t *testing.T) {
+func TestDropNoRoot(t *testing.T) {
 	ConfigRoot = ""
 	actual := new(bytes.Buffer)
 	RootCmd.SetOut(actual)
-	RootCmd.SetArgs([]string{"diff"})
+	RootCmd.SetArgs([]string{"drop"})
 	err := RootCmd.Execute()
 
 	assert.ErrorContains(t, err, "Configuration file root does not exists")
 }
 
-func TestDiffRoot(t *testing.T) {
+func TestDropRoot(t *testing.T) {
 	ConfigRoot = ""
 	actual := new(bytes.Buffer)
 	RootCmd.SetOut(actual)
-	RootCmd.SetArgs([]string{"diff", "-r", "/tmp"})
+	RootCmd.SetArgs([]string{"drop", "-r", "/tmp"})
 	err := RootCmd.Execute()
 
 	assert.Nil(t, err)
